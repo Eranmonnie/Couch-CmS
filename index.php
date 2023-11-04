e9c7adde5d909fcb510b7bb74ba7a9a7e177daa8
Eranmonnie <ajalaferanmi123@gmail.com>
2023-11-04 15:30:41 +0100

    home page

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 require_once ('couch/cms.php');
 ?>
 
+<cms:template title='Home' icon='home'>
+    <cms:editable name="body" label="Body" type="richtext" />
+    <cms:editable name="excerpt" label="Excerpt" type="textarea" />
+</cms:template>
 
 <h1>hergerhg</h1>
 
